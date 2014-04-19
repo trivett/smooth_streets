c716fcd3c98071b576be3136dda5032770fc55d1
Justin Tanacredi <Jtanacredi@gmail.com>
2014-04-18 23:05:36 -0400

    begin crack setup in potholes model

--- app/models/pothole.rb
+++ app/models/pothole.rb
@@ -1,9 +1,11 @@
-class Pothole << ActiveRecord::Base
+class Pothole < ActiveRecord::Base
   belongs_to :community
   validates :unique_key, presence: true
   validates :zipcode, presence: true
   validates :community_id, numericality: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+
+ parsed = Crack::JSON.parse()
 
 end
