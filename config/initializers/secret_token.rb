0b29fa2f1403952f703cd77a7f4ed2dbb857bc9d
Vincent Trivett <trivett@gmail.com>
2014-04-18 20:20:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmoothStreets::Application.config.secret_key_base = '3b2e2bb5f50c72b863afef5d46ab6b7623d4ca813c33bb673c5a695cd0949cadf2169b924c2bb770d12bf35e26cbddee0c3ba87533332de7c2bd464d16339da6'
